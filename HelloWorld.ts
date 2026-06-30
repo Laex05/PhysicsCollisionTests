332a5d25c4de6231ab273b20794543d42b14b759
Alex Chandler <alex.d.chandler@gmail.com>
2026-06-29 20:32:41 -0700

    More tests

--- HelloWorld.ts
+++ HelloWorld.ts
@@ -15,11 +15,21 @@ function start() {
 
   console.log('Hello World!');
 
-  const cube1 = spawnPrimitive.cube(new Vector3(0, 0.35, -2), new Vector3(0.5, 0.5, 0.5), Quaternion.one, Color.pink, 0.45, true, 'Animated', undefined, true);
-  const cube2 = spawnPrimitive.cube(new Vector3(0, 0.35, -4), new Vector3(0.5, 0.5, 0.5), Quaternion.one, Color.purple, 0.45, true, 'Animated', undefined, false);
+  const cube1 = spawnPrimitive.cube(new Vector3(0, 0.35, -2), new Vector3(0.5, 0.5, 0.5), Quaternion.one, Color.pink, 0.7, true, 'Animated', undefined, true);
+  const cube1_2 = spawnPrimitive.cube(new Vector3(0, 1.1, -4), new Vector3(0.5, 2, 0.5), Quaternion.one, Color.pink, 0.7, true, 'Animated', undefined, true);
+  const cube2 = spawnPrimitive.cube(new Vector3(0, 0.35, -6), new Vector3(0.5, 0.5, 0.5), Quaternion.one, Color.purple, 0.45, true, 'Animated', undefined, false);
+  const cube2_2 = spawnPrimitive.cube(new Vector3(0, 1.1, -8), new Vector3(0.5, 2, 0.5), Quaternion.one, Color.purple, 0.45, true, 'Animated', undefined, false);
+  const cube3 = spawnPrimitive.cube(new Vector3(0, 0.35, -10), new Vector3(0.5, 0.5, 0.5), Quaternion.one, Color.lavender, 0.55, true, 'Static', undefined, true);
+  const cube3_2 = spawnPrimitive.cube(new Vector3(0, 1.1, -12), new Vector3(0.5, 2, 0.5), Quaternion.one, Color.lavender, 0.55, true, 'Static', undefined, true);
+  const cube4 = spawnPrimitive.cube(new Vector3(0, 0.35, -14), new Vector3(0.5, 0.5, 0.5), Quaternion.one, Color.cyan, 0.55, true, 'Static', undefined, false);
+  const cube4_2 = spawnPrimitive.cube(new Vector3(0, 1.1, -16), new Vector3(0.5, 2, 0.5), Quaternion.one, Color.cyan, 0.55, true, 'Static', undefined, false);
 
   moveBackAndForth(cube1);
+  moveBackAndForth(cube1_2);
   moveBackAndForth(cube2);
+  moveBackAndForth(cube2_2);
+  moveBackAndForth(cube3);
+  moveBackAndForth(cube3_2);
 }
 
 async function moveBackAndForth(entity: Entity) {
@@ -31,7 +41,6 @@ async function moveBackAndForth(entity: Entity) {
   const endPos = startPos.add(new Vector3(3, 0, 0));
 
   while (i > 0) {
-    console.log(i);
     if (i % 2 === 0) {
       overTime.moveTo.start(entity, startPos, i * 1_000);
     }
